3e83b71c70f1fa77c7bd6ba5134d0c5fb9cce8fe
tsanbumozhy <tsanbumozhy@gmail.com>
2023-06-23 11:36:16 +0530

    Finding the defects for Level-1 problem considering the entire image as a single die

--- code/solution.py
+++ code/solution.py
@@ -1,13 +1,81 @@
+import os
+import csv
 import json
+from PIL import Image
 
-with open(r'C:/Users/Anbumozhy/Desktop/KLA/Level_1_Input_Data/input.json') as json_File :
-	data=json.load(json_File)
+def findMajority(lst):
+	maxCount = 0
+	index = -1
+	for i in range(len(lst)):
+		count = 1
+		for j in range(i+1, len(lst)):
+			if(lst[i] == lst[j]):
+				count += 1
+
+		if(count > maxCount):
+			maxCount = count
+			index = i
+
+	if (maxCount > len(lst)//2):
+		return(lst[index])
+
+	else:
+		return('none')
 
 
-die = data['die']
+if __name__ == "__main__":
 
-streetWidth = data['street_width']
+    with open(r'C:/Users/Anbumozhy/Desktop/KLA/Level_1_Input_Data/input.json') as json_File :
+        data=json.load(json_File)
 
-careAreaDimentions = data['care_areas']
 
-exclusionZones = data['exclusion_zones']
+    die = data['die']
+
+    streetWidth = data['street_width']
+
+    careAreaDimentions = data['care_areas']
+
+    exclusionZones = data['exclusion_zones']
+
+
+    dirPath = r'C:/Users/Anbumozhy/Desktop/KLA/Level_1_Input_Data'
+
+    imagePaths = [entry for entry in os.listdir(dirPath) if os.path.isfile(os.path.join(dirPath, entry))]
+
+    imagePaths.remove('input.json')
+
+    pixelImages = []
+
+    width, height = die['width'], die['height']
+
+    for imgInput in imagePaths:
+        image = Image.open(dirPath+'/'+imgInput, 'r')
+        pixelImages.append(image.load())
+
+    anomalies = []
+
+    for y in range(height):
+        for x in range(width):
+            pixelRows = []
+            pixelDimension = []
+            for pixel in range(len(pixelImages)):
+                r, g, b = pixelImages[pixel][x, y]
+
+                pixelRows.append(f'#{r:02x}{g:02x}{b:02x}')
+                pixelDimension.append((pixel+1, x, y))
+		
+            majority = findMajority(pixelRows)
+
+            if majority != 'none':
+                for pixel in range(len(pixelRows)):
+                    if pixelRows[pixel] != majority:
+                        anomalies.append(pixelDimension[pixel])
+            else:
+                for pixel in range(len(pixelRows)):
+                    anomalies.append(pixelDimension[pixel])
+
+    with open('output.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+
+        for anomaly in anomalies:
+            writer.writerow(anomaly)
